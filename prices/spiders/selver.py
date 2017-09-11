56d2cda0a1f21951cee727fefc0340ac11b3f97b
Valery Ivanov <valeri_ivanov35@hotmail.com>
2017-09-11 10:10:10 +0300

    selver spider additions

--- prices/spiders/selver.py
+++ prices/spiders/selver.py
@@ -12,8 +12,9 @@ class SelverSpider(scrapy.Spider):
     def parse_products(self, response):
         for quote in response.css('#products-grid li'):
             yield {
-                # TODO price not always fetched
-                'price': quote.css('.left span.price::text').extract_first(),
+                'img': quote.css('a img::attr("src")').extract_first(),
+                'price': quote.css('.left .price::text').extract_first(),
+                'unitprice': quote.css('.left .unit-price::text').extract_first(),
                 'product': quote.css('h5.product-name a::text').extract_first(),
             }
         next_page = response.css('ol.pagination li a.next::attr("href")').extract_first()
